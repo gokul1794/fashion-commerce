31e744c0b6ee5a7cc13a964106e064acf134df0d
Gokul Shanth <gokul.shanth@gmail.com>
2020-04-23 12:37:49 -0400

    Added nav bar, created random categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'home' , component: HomePageComponent},
+  {path: 'productlist', component: ProductListingComponent},
+  {path: 'details', component: ProductDetailsComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProductListingComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatGridListModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
